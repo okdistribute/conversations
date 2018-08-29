db2d8c01162ffaed3716d2a6b52f84f2c21a738c
Karissa McKelvey <krmckelv@gmail.com>
2018-08-29 11:33:49 -0700

    remove js files

--- ts/notifications/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getStatus_1 = require("./getStatus");
-exports.getStatus = getStatus_1.getStatus;
--- ts/types/IndexedDB.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.INDEXABLE_FALSE = 0;
-exports.INDEXABLE_TRUE = 1;
-exports.toIndexableBoolean = (value) => value ? exports.INDEXABLE_TRUE : exports.INDEXABLE_FALSE;
-exports.toIndexablePresence = (value) => value ? exports.INDEXABLE_TRUE : undefined;
